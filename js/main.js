a3316c9dd2132e885dbaa26e464ec3697d233496
sofia camelo <camelojimenezsofia20@gmail.com>
2025-09-18 13:20:59 -0500

    estructura inicial

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// Creamos las constantes globales para los elementos principales 
+
+const input =document.getElementById("to-do-input");
+const addbtn = document.getElementById("add-btn");
+const ToDoList = document.getElementById("cont-to-do-list");
+const CompletedList = document.getElementById("cont-completed");
+
+// Creamos la función que nos permite crear una nueva tarea 
+// Todas las etiquetas que vamos a crear parten de la maqueta HTML preexistente
+
+function createToDoItem(textoItem){
+    // Creamos el nodo o elemento padre
+    const item =document.createElement("div");
+    item.classList.add("item-to-do");
+
+    // Creamos el nodo o elemento hijo y le agregamos el nodo checkbox
+    const checkbox=document.createElement("input");
+    checkbox.type="checkbox";
+
+    // Creamos el siguiente nodo hijo (párrafo). 
+    // A este párrafo le asignamos el valor del argumento de la función, 
+    // es decir, lo que escribe el usuario en el campo
+    const p =document.createElement("p");
+    p.textContent=textoItem;
+
+    // Creamos el último nodo hijo: el botón de eliminar
+    const deleteBtn=document.createElement("button");
+    deleteBtn.textContent="x";
+
+    // Ensamblamos dentro del nodo padre sus nodos hijos, es decir, la estructura de la tarea
+    item.appendChild(checkbox);
+    item.appendChild(p);
+    item.appendChild(deleteBtn);
+
+    // Utilizamos return para devolver el elemento creado, ya que lo usaremos en otra función más adelante
+    return item;
+} 
+
+// Detectamos el evento click sobre el botón "Agregar" con un event listener
+// A partir de este evento se agregará la tarea dentro del contenedor cont-to-do-list
+
+addbtn.addEventListener('click', ()=>{
+    const textoItem=input.value.trim();
+    if (textoItem=="") {
+        alert("No se puede crear una tarea vacia")
+    } else {
+        const newItem = createToDoItem(textoItem);
+        ToDoList.appendChild(newItem);
+        input.value="";
+    }
+});
